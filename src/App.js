9b19681a94b13b1ae0a7ca99c5476c75158a327c
smfischer87 <smbansmer@gmail.com>
2021-09-04 16:38:11 -0700

    added gitbub repo link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Susan Fischer and is{" "}
+        <a href="https://github.com/smfischer87/react-weather-app-pro" target="_blank">open-sourced</a>{" "}on Github.
+      </footer>
     </div>
   );
 }
