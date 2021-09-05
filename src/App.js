c78ea431b794930aceb439597ab02ea1697d64db
smfischer87 <smbansmer@gmail.com>
2021-09-04 17:01:50 -0700

    created weather html structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project was coded by Susan Fischer and is{" "}
         <a href="https://github.com/smfischer87/react-weather-app-pro" rel="noreferrer" target="_blank">open-sourced</a>{" "}on Github.
       </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./weather.css";
+
+export default function Weather() {
+    return (
+        <div className="weather">
+            Hello from Weather</div>
+    )
+}
