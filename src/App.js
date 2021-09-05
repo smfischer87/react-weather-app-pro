1d95b0f40c9b3098f7be4241b538843da2807ee8
smfischer87 <smbansmer@gmail.com>
2021-09-04 17:25:23 -0700

    created search form and result structure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
       <Weather />
       <footer>
         This project was coded by Susan Fischer and is{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,36 @@ import "./weather.css";
 export default function Weather() {
     return (
         <div className="weather">
-            Hello from Weather</div>
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                        <input type="search" placeholder="Enter a City..." 
+                className="form-control"/>
+                    </div>
+                <div className="col-3">
+                    <input type="submit" value="Search" className="btn btn-primary" />
+                </div>
+                
+                </div>
+            </form>
+            <h1>New York</h1>
+            <ul>
+                <li>Wednesday 07:00</li>
+                <li>Mostly Cloudy</li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="sunny and cloudy" />
+                    73°F
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Precipitation: 1%</li>
+                        <li>Humidity: 65%</li>
+                        <li>Wind: 6 mph</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
