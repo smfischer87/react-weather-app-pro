145cfdeebc90bb98a7780900c6a317cd0daa4535
smfischer87 <smbansmer@gmail.com>
2021-09-11 11:09:39 -0700

    cleaned up code

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,10 +2,17 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay (props) {
+    function day() {
+        let date = new Date(props.data.dt * 1000);
+        let day = date.getDay();
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        return days[day];
+    }
     return (
         <div>
             <div className="WeatherForecast-day">
-                {props.data.dt}
+                {day()}
             </div>
             <WeatherIcon code={props.data.weather[0].icon} size={36} />
             <div className="WeatherForecast-temperatures">
